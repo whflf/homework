605db18c1f2b797b6e7839ea7491c2efbd1b0e9a
whflf <fluffy.jesus@yandex.ru>
2023-10-25 18:12:19 +0300

    fix sth

--- Smart Qsort/main.c
+++ Smart Qsort/main.c
@@ -2,52 +2,46 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-int scanfSafetyChecks(int scanfFieldsFilled)
-{
-    char* extraInput = { '\0' };
+#define RETURN_OK 0
+#define RETURN_TESTS_FAILED 1
+#define RETURN_OUT_OF_MEMORY 2
 
-    if (!scanfFieldsFilled) {
-        printf("!! Wrong input. Please enter an integer but nothing else !!");
-        return 1;
-    }
-    if (scanf_s("%c", &extraInput) != EOF && extraInput != '\n') {
-        printf("!! Extra characters found in input. Please enter an integer and nothing else !!");
-        return 2;
-    }
-    return 0;
-}
-
-void swap(int* value1, int* value2)
+void swap(int* const value1, int* const value2)
 {
-    int value1Prev = *value1;
+    const int value1Prev = *value1;
     *value1 = *value2;
     *value2 = value1Prev;
 }
 
-void insertionSort(int* array, int low, int high)
+void insertionSort(int* const array, const int low, const int high)
 {
-    for (int i = low; i <= high - 1; ++i) {
-        if (i == low && low == 0) 
+    for (size_t i = low; i <= high - 1; ++i)
+    {
+        if (i == low && low == 0)
         {
             continue;
         }
         int j = i - 1;
-        while (array[j + 1] < array[j]) {
+        while (array[j + 1] < array[j])
+        {
             swap(&array[j + 1], &array[j]);
             --j;
         }
     }
 }
 
-int partition(int array[], int low, int high)
+int partition(int array[], const int low, const int high)
 {
-    int pivot = array[high];
-    int i = (low - 1);
+    const int pivot = array[high];
+    size_t i = (low - 1);
 
-    for (int j = low; j <= high - 1; j++) {
-        if (array[j] <= pivot) {
+    for (size_t j = low; j <= high - 1; ++j)
+    {
+        if (array[j] <= pivot)
+        {
             ++i;
-            if (i != j) {
+            if (i != j)
+            {
                 swap(&array[i], &array[j]);
             }
         }
@@ -57,20 +51,25 @@ int partition(int array[], int low, int high)
     return (i + 1);
 }
 
-void quicksort(int array[], int low, int high)
+void quicksort(int array[], const int low, const int high)
 {
-    if (low < high) {
-        int partitionIndex = partition(array, low, high);
-        if ((partitionIndex - low) >= 10) {
+    if (low < high)
+    {
+        const int partitionIndex = partition(array, low, high);
+        if ((partitionIndex - low) >= 10)
+        {
             quicksort(array, low, partitionIndex);
         }
-        else {
+        else
+        {
             insertionSort(array, low, partitionIndex + 1);
         }
-        if ((high - partitionIndex) >= 10) {
+        if ((high - partitionIndex) >= 10)
+        {
             quicksort(array, partitionIndex + 1, high);
         }
-        else {
+        else
+        {
             insertionSort(array, partitionIndex, high + 1);
         }
     }
@@ -78,61 +77,62 @@ void quicksort(int array[], int low, int high)
 
 bool testCorrectCase()
 {
-    int unsortedArray[5] = { 2, 5, 4, 3, 1 };
-    int sortedArray[5] = { 1, 2, 3, 4, 5 };
+    const int unsortedArray[5] = { 2, 5, 4, 3, 1 };
+    const int sortedArray[5] = { 1, 2, 3, 4, 5 };
     quicksort(unsortedArray, 0, 4);
     return memcmp(unsortedArray, sortedArray, 5) == 0;
 }
 
 bool testBoundaryCase()
 {
-    int unsortedArray[5] = { 1, 2, 3, 4, 5 };
-    int sortedArray[5] = { 1, 2, 3, 4, 5 };
+    const int unsortedArray[5] = { 1, 2, 3, 4, 5 };
+    const int sortedArray[5] = { 1, 2, 3, 4, 5 };
     quicksort(unsortedArray, 0, 4);
     return memcmp(unsortedArray, sortedArray, 5) == 0;
 }
 
-int main() {
+int main()
+{
     if (!testCorrectCase() || !testBoundaryCase())
     {
         printf("~ Tests failed");
-        return -1;
+        return RETURN_TESTS_FAILED;
     }
 
-    int size = 0;
+    size_t size = 0;
 
     printf("> Enter array size: ");
 
-    if (scanfSafetyChecks(scanf_s("%d", &size))) {
-        return 1;
-    }
+    fgets(&size, sizeof(size_t) + 1, stdin);
 
-    if (size < 0) {
-        printf("!! Array size can not be negative !!");
-        return 1;
-    }
+    size = atoi(&size);
 
     int* numbers = (int*)malloc(size * sizeof(int));
-    for (int i = 0; i < size; ++i)
+    if (numbers == NULL) 
+    {
+        printf("Memory allocation has failed :(");
+        return RETURN_OUT_OF_MEMORY;
+    }
+    for (size_t i = 0; i < size; ++i)
     {
         printf("> a[%d] = ", i);
-
-        if (scanfSafetyChecks(scanf_s("%d", &numbers[i]))) {
-            return 1;
-        }
+        fgets(&numbers[i], sizeof(int) + 1, stdin);
+        numbers[i] = atoi(&numbers[i]);
     }
 
     quicksort(numbers, 0, size - 1);
 
     printf("Result: ");
-    for (int i = 0; i < size; ++i) {
+    for (size_t i = 0; i < size; ++i)
+    {
         printf("%d", numbers[i]);
-        if (i < size - 1) {
-            printf(",");
+        if (i < size - 1)
+        {
+            printf(", ");
         }
     }
     printf("\n");
 
     free(numbers);
-    return 0;
+    return RETURN_OK;
 }
