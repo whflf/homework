8fb235dd0c46a7a059a3ffadfc72a97284ee3959
whflf <fluffy.jesus@yandex.ru>
2023-10-27 15:24:53 +0300

    add char count

---
+++ Symbols count/main.c
@@ -0,0 +1,90 @@
+﻿#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#define FILE_OPEN_ERROR -1
+#define OK 0
+#define OUT_OF_MEMORY 1
+#define TESTS_FAILED 2
+
+
+int charCount(int* const array, char* const filename)
+{
+    FILE* file;
+    if (fopen_s(&file, filename, "r") != 0)
+    {
+        return FILE_OPEN_ERROR;
+    }
+
+    while (!feof(file))
+    {
+        const int character = fgetc(file);
+        ++array[character];
+    }
+
+    fclose(file);
+}
+
+bool test1() 
+{
+    int* const characters = (int*)calloc(256, sizeof(int) * 256);
+    if (characters == NULL) 
+    {
+        return 0;
+    }
+    if (charCount(characters, "test1.txt") != FILE_OPEN_ERROR) 
+    {
+        free(characters);
+        return characters[10] == 13;
+    }
+
+    free(characters);
+    return 0;
+}
+
+bool test2()
+{
+    int* const characters = (int*)calloc(256, sizeof(int) * 256);
+    if (characters == NULL)
+    {
+        return 0;
+    }
+    if (charCount(characters, "test2.txt") != FILE_OPEN_ERROR)
+    {
+        free(characters);
+        return characters[32] == 15 && characters[78] == 1 && characters[10] == 0 && characters[111] == 8;
+    }
+
+    free(characters);
+    return 0;
+}
+
+int main()
+{
+    if (!test1() || !test2()) {
+        printf("~ Tests have failed");
+        return TESTS_FAILED;
+    }
+
+    int* const characters = (int*)calloc(256, sizeof(int) * 256);
+    if (characters == NULL)
+    {
+        printf("~ Memory allocation has failed");
+        return OUT_OF_MEMORY;
+    }
+
+    if (charCount(characters, "file.txt") != FILE_OPEN_ERROR) {
+    charCount(characters, "file.txt");
+
+        for (size_t i = 0; i < 256; ++i) {
+            if (characters[i] == 0)
+            {
+                continue;
+            }
+            printf("%c - %d\n", (char)i, characters[i]);
+        }
+    }
+
+    free(characters);
+    return OK;
+}
