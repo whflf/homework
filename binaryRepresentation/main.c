58204c42893a816dd8c66d44cebb3f92ed3c1ac3
whflf <fluffy.jesus@yandex.ru>
2023-11-18 00:36:50 +0300

    fix

--- binaryRepresentation/main.c
+++ binaryRepresentation/main.c
@@ -11,199 +11,200 @@
 #define WRONG_SCANF_FIELDS_ERROR 1
 #define EXTRA_INPUT_ERROR 2
 #define OUT_OF_MEMORY 3
+#define FAILED_TO_ALLOCATE_MEMORY_TEXT "~Не удалось выделить память.\n"
 
-int scanfSafetyChecks(int scanfFieldsFilled)
+int scanfSafetyChecks(const int scanfFieldsFilled)
 {
-	char extraInput = 0;
+    const char extraInput = 0;
 
-	if (!scanfFieldsFilled)
-	{
-		printf("!! Неверный ввод. Пожалуйста, введите целое число !!");
-		return WRONG_SCANF_FIELDS_ERROR;
-	}
-	if (scanf("%c", &extraInput) != EOF && extraInput != '\n')
-	{
-		printf("!! Пожалуйста, введите целое число без лишних знаков !!");
-		return EXTRA_INPUT_ERROR;
-	}
-	return OK;
+    if (!scanfFieldsFilled)
+    {
+        printf("!! Неверный ввод. Пожалуйста, введите целое число !!");
+        return WRONG_SCANF_FIELDS_ERROR;
+    }
+
+    if (scanf("%c", &extraInput) != EOF && extraInput != '\n')
+    {
+        printf("!! Пожалуйста, введите целое число без лишних знаков !!");
+        return EXTRA_INPUT_ERROR;
+    }
+
+    return OK;
 }
 
-char* const binary(int x)
+char* binary(int x)
 {
-	if (x < 0)
-	{
-		x = (1 << DIGITS) + x;
-	}
+    if (x < 0)
+    {
+        x = (1 << DIGITS) + x;
+    }
 
-	char* const binaryNumber = (char*)calloc(DIGITS, sizeof(char));
-	if (binaryNumber == NULL)
-	{
-		return NULL;
-	}
+    char* const binaryNumber = (char*)calloc(DIGITS, sizeof(char));
+    if (binaryNumber == NULL)
+    {
+        return NULL;
+    }
 
-	int bit = 1 << DIGITS - 1;
+    int bit = 1 << DIGITS - 1;
 
-	for (int i = 0; i < DIGITS; ++i)
-	{
-		if (x & bit)
-		{
-			binaryNumber[i] = 1;
-		}
-		else
-		{
-			binaryNumber[i] = 0;
-		}
-		bit = bit >> 1;
-	}
+    for (int i = 0; i < DIGITS; ++i)
+    {
+        binaryNumber[i] = x & bit ? 1 : 0;
+        bit = bit >> 1;
+    }
 
-	return binaryNumber;
+    return binaryNumber;
 }
 
-char* const binarySum(char* const x, char* const y)
+char* binarySum(const char* const x, const char* const y)
 {
-	char* const binarySum = (int*)calloc(DIGITS, sizeof(char));
-	if (binarySum == NULL)
-	{
-		return NULL;
-	}
+    char* const binarySum = (char*)calloc(DIGITS, sizeof(char));
+    if (binarySum == NULL)
+    {
+        return NULL;
+    }
 
-	char additionalDigit = 0;
+    char additionalDigit = 0;
 
-	for (int i = DIGITS - 1; i >= 0; --i)
-	{
-		int sum = x[i] + y[i] + additionalDigit;
-		if (sum > 1)
-		{
-			if (sum == 3)
-			{
-				binarySum[i] = 1;
-			}
-			additionalDigit = 1;
-		}
-		else
-		{
-			binarySum[i] = sum;
-			additionalDigit = 0;
-		}
-	}
+    for (int i = DIGITS - 1; i >= 0; --i)
+    {
+        const int sum = x[i] + y[i] + additionalDigit;
+        binarySum[i] = sum % 2;
+        additionalDigit = sum / 2;
+    }
 
-	return binarySum;
+    return binarySum;
 }
 
-int decimal(char* const x)
+int decimal(const char* const x)
 {
-	int decimalNumber = 0;
+    int decimalNumber = 0;
 
-	for (int i = 0; i < DIGITS; ++i)
-	{
-		int degree = DIGITS - 1 - i;
-		decimalNumber += (1 << degree) * x[i];
-	}
+    for (int i = 0; i < DIGITS; ++i)
+    {
+        int degree = DIGITS - 1 - i;
+        decimalNumber += (1 << degree) * x[i];
+    }
 
-	if (x[0] == 1)
-	{
-		decimalNumber = -((1 << DIGITS) - decimalNumber);
-	}
-	return decimalNumber;
+    if (x[0] == 1)
+    {
+        decimalNumber = -((1 << DIGITS) - decimalNumber);
+    }
+    return decimalNumber;
 }
 
-void printBinary(char* const x)
+void printBinary(const char* const x)
 {
-	for (size_t i = 0; i < DIGITS; ++i)
-	{
-		printf("%d", x[i]);
-	}
+    for (size_t i = 0; i < DIGITS; ++i)
+    {
+        printf("%d", x[i]);
+    }
 }
 
-bool testPositiveCase()
+bool sumTest(const int x, const int y, const char* const expectedBinaryValue, const int expectedIntValue)
 {
-	int x = 5, y = 8;
-	char* binaryX = binary(x),
-		* binaryY = binary(y),
-		* sumOfXAndY = binarySum(binaryX, binaryY);
-	char correctSum[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1 };
-	bool result = memcmp(sumOfXAndY, correctSum, DIGITS) == 0 && decimal(sumOfXAndY) == 13;
-	free(binaryX);
-	free(binaryY);
-	free(sumOfXAndY);
-	return result;
+    const char* const binaryX = binary(x);
+    const char* const binaryY = binary(y);
+    const char* const sumOfXAndY = binarySum(binaryX, binaryY);
+
+    if (binaryX == NULL || binaryY == NULL || sumOfXAndY == NULL)
+    {
+        free(binaryX);
+        free(binaryY);
+        free(sumOfXAndY);
+        printf(FAILED_TO_ALLOCATE_MEMORY_TEXT);
+        return false;
+    }
+
+    bool result = memcmp(sumOfXAndY, expectedBinaryValue, DIGITS) == 0 && decimal(sumOfXAndY) == expectedIntValue;
+
+    free(binaryX);
+    free(binaryY);
+    free(sumOfXAndY);
+
+    return result;
 }
 
-bool testNegativeCase()
+bool testPositiveCase(void)
 {
-	int x = 5, y = -6;
-	char* binaryX = binary(x),
-		* binaryY = binary(y),
-		* sumOfXAndY = binarySum(binaryX, binaryY);
-	char correctSum[] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
-	bool result = memcmp(sumOfXAndY, correctSum, DIGITS) == 0 && decimal(sumOfXAndY) == -1;
-	free(binaryX);
-	free(binaryY);
-	free(sumOfXAndY);
-	return result;
+    int const x = 5, const y = 8;
+
+    const char correctSumBits[] = {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1
+    };
+
+    const int correctSum = 13;
+
+    return sumTest(x, y, correctSumBits, correctSum);
+}
+
+bool testNegativeCase(void)
+{
+    int const x = 5, const y = -6;
+
+    const char correctSumBits[] = {
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+    };
+
+    const int correctSum = -1;
+
+    return sumTest(x, y, correctSumBits, correctSum);
 }
 
 int main(void)
 {
-	if (!testPositiveCase() && !testNegativeCase())
-	{
-		printf("~Tests failed");
-		return TESTS_FAILED;
-	}
+    setlocale(LC_ALL, "ru-RU");
 
-	setlocale(LC_ALL, "ru-RU");
+    if (!testPositiveCase() || !testNegativeCase())
+    {
+        printf("~ Тесты провалены.\n");
+        return TESTS_FAILED;
+    }
 
-	int x = 0, y = 0;
+    int scanfError,
+        x = 0, y = 0;
 
-	printf("> Введите х: ");
-	int scanfError;
-	if (scanfError = scanfSafetyChecks(scanf_s("%d", &x)))
-	{
-		return scanfError;
-	}
+    printf("> Введите х: ");
+    if (scanfError = scanfSafetyChecks(scanf("%d", &x)))
+    {
+        return scanfError;
+    }
 
-	printf("> Введите у: ");
-	if (scanfError = scanfSafetyChecks(scanf_s("%d", &y)))
-	{
-		return scanfError;
-	}
+    printf("> Введите у: ");
+    if (scanfError = scanfSafetyChecks(scanf("%d", &y)))
+    {
+        return scanfError;
+    }
 
-	char* const binaryX = binary(x);
-	if (binaryX == NULL)
-	{
-		printf("~ Memory allocation has failed");
-		return OUT_OF_MEMORY;
-	}
-	char* const binaryY = binary(y);
-	if (binaryY == NULL)
-	{
-		printf("~ Memory allocation has failed");
-		free(binaryX);
-		return OUT_OF_MEMORY;
-	}
+    const char* const binaryX = binary(x);
+    const char* const binaryY = binary(y);
+    const char* const sumOfXAndY = binarySum(binaryX, binaryY);
 
-	printf("Двоичное представление числа х: ");
-	printBinary(binaryX);
-	printf("\nДвоичное представление числа у: ");
-	printBinary(binaryY);
+    if (binaryX == NULL || binaryY == NULL || sumOfXAndY == NULL)
+    {
+        free(binaryX);
+        free(binaryY);
+        free(sumOfXAndY);
+        printf(FAILED_TO_ALLOCATE_MEMORY_TEXT);
+        return OUT_OF_MEMORY;
+    }
 
-	printf("\nДвоичное представление суммы чисел х и у: ");
-	char* const sumOfXAndY = binarySum(binaryX, binaryY);
-	if (sumOfXAndY == NULL)
-	{
-		printf("~ Memory allocation has failed");
-		free(binaryX);
-		free(binaryY);
-		return OUT_OF_MEMORY;
-	}
-	printBinary(sumOfXAndY);
+    printf("Двоичное представление числа х: ");
+    printBinary(binaryX);
 
-	printf("\nДесятичное представление суммы чисел х и у: %d", decimal(sumOfXAndY));
+    printf("\nДвоичное представление числа у: ");
+    printBinary(binaryY);
 
-	free(binaryX);
-	free(binaryY);
-	free(sumOfXAndY);
+    printf("\nДвоичное представление суммы чисел х и у: ");
+    printBinary(sumOfXAndY);
 
-	return OK;
+    printf("\nДесятичное представление суммы чисел х и у: %d\n", decimal(sumOfXAndY));
+
+    free(binaryX);
+    free(binaryY);
+    free(sumOfXAndY);
+
+    return OK;
 }
