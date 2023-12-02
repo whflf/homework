0af430a7390f9cf2ee40522e8d7dfc25009c24e1
whflf <fluffy.jesus@yandex.ru>
2023-12-02 18:53:24 +0300

    fix

---
+++ Postfix Calculator/include/errors.c
@@ -0,0 +1,19 @@
+#include "errors.h"
+
+char* getErrorMessage(const ErrorCode errorCode)
+{
+    switch (errorCode)
+    {
+    case stackIsEmpty:
+        return "Stack is empty.";
+    case outOfMemory:
+        return "Memory allocation has failed.";
+    case divisionByZero:
+        return "Division by zero is forbidden. Please enter a valid expression.";
+    case badInput:
+        return "The expression isn't correct.";
+    case testsFailed:
+        return "~ Tests failed.";
+    }
+    return "Unknown error.";
+}
