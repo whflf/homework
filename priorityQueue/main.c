4009ce4f632f6887caea2c0ee60ecb57b20f3a81
whflf <fluffy.jesus@yandex.ru>
2023-12-13 15:24:01 +0300

    add task files

---
+++ priorityQueue/main.c
@@ -0,0 +1,58 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "queue.h"
+
+bool test1(void)
+{
+    Queue* priorityQueue = createQueue();
+
+    ErrorCode errors[5] = { enqueue(&priorityQueue, 1, 3),
+    enqueue(&priorityQueue, 2, 1),
+    enqueue(&priorityQueue, 3, 2),
+    enqueue(&priorityQueue, 4, 5),
+    enqueue(&priorityQueue, 5, 4) };
+
+    for (size_t i = 0; i < 5; ++i)
+    {
+        if (errors[i] != ok)
+        {
+            deleteQueue(&priorityQueue);
+            return false;
+        }
+    }
+
+    int* comparativeArray[] = {2, 3, 1, 5, 4};
+
+    for (size_t i = 0; i < 5; ++i)
+    {
+        if (dequeue(priorityQueue) != comparativeArray[i])
+        {
+            deleteQueue(&priorityQueue);
+            return false;
+        }
+    }
+
+    deleteQueue(&priorityQueue);
+    return true;
+}
+
+bool test2(void)
+{
+    Queue* priorityQueue = createQueue();
+
+    return dequeue(priorityQueue) == -1;
+}
+
+int main(void)
+{
+    if (!test1() || !test2())
+    {
+        printf("~Tests failed.\n");
+        return testsFailed;
+    }
+
+    printf("OK\n");
+    return ok;
+}
