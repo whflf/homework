b7e3a61d547419a95dd7f43f7f73457bd7a32286
whflf <fluffy.jesus@yandex.ru>
2023-12-27 14:11:21 +0300

    add task files

---
+++ test2.3task2/main.c
@@ -0,0 +1,12 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+
+#include "tests.h"
+#include "errors.h"
+
+int main(void)
+{
+    const ErrorCode error = passTests() ? ok : testsFailed;
+    return printErrorMessage(error);
+}
+
