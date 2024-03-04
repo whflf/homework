5f4690bb80187a003d683c44d2bf78f73599ef19
whflf <fluffy.jesus@yandex.ru>
2024-03-04 20:47:50 +0300

    add task files

---
+++ Trie/Program.cs
@@ -0,0 +1,40 @@
+﻿Trie myTrie = new Trie();
+
+bool TestAdding()
+{
+    string[] stringArray = new string[4] { "he", "she", "his", "hers" };
+    foreach (string str in stringArray)
+    {
+        if (!myTrie.Add(str))
+        {
+            return false;
+        }
+    }
+
+    return !myTrie.Add("his");
+}
+
+bool TestContaining()
+{
+    return myTrie.Contains("hers") && !myTrie.Contains("hello");
+}
+
+bool TestRemoving()
+{
+    return myTrie.Remove("he") && !myTrie.Remove("hello");
+}
+
+bool TestCounting()
+{
+    return myTrie.HowManyStartsWithPrefix("h") == 2 && myTrie.HowManyStartsWithPrefix("o") == 0;
+}
+
+if (!TestAdding() && !TestContaining() && !TestRemoving() && !TestCounting())
+{
+    Console.WriteLine("~ Tests failed");
+}
+else
+{
+    myTrie = new Trie();
+    MenuInterface.ProgramLoop(myTrie);
+}
---
+++ Trie/Trie.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Trie
+{
+    private Trie?[] next;
+    private bool isTerminal;
+    private int size;
+
+    public Trie()
+    {
+        this.next = new Trie[256];
+    }
+
+    public bool Add(string element, int position = 0)
+    {
+        if (position == element.Length)
+        {
+            if (this.isTerminal)
+            {
+                return false;
+            }
+
+            this.isTerminal = true;
+            return true;
+        }
+
+        bool newVertexCreated = false;
+        if (this.next[element[position]] == null)
+        {
+            this.next[element[position]] = new Trie();
+            ++this.size;
+            newVertexCreated = true;
+
+            if (position == element.Length - 1)
+            {
+                this.next[element[position]].isTerminal = true;
+                return true;
+            }
+        }
+
+        bool isNewElement = this.next[element[position]].Add(element, ++position);
+        if (isNewElement && !newVertexCreated)
+        {
+            ++this.size;
+        }
+
+        return isNewElement;
+    }
+
+    public bool Contains(string element, int position = 0)
+    {
+        if (position == element.Length)
+        {
+            return this.isTerminal;
+        }
+
+        if (this.next[element[position]] == null)
+        {
+            return false;
+        }
+
+        return this.next[element[position]].Contains(element, ++position);
+    }
+
+    public bool Remove(string element, int position = 0)
+    {
+        if (this.next[element[position]] == null)
+        {
+            return false;
+        }
+
+        if (position == element.Length - 1)
+        {
+            if (!this.next[element[position]].isTerminal)
+            {
+                return false;
+            }
+
+            this.next[element[position]] = null;
+            --this.size;
+            return true;
+        }
+
+        return this.next[element[position]].Remove(element, ++position);   
+    }
+
+    public int HowManyStartsWithPrefix(string prefix, int position = 0)
+    {
+        if (position == prefix.Length)
+        {
+            if (this.isTerminal)
+            {
+                return this.size + 1;
+            }
+
+            return this.size;
+        }
+
+        if (this.next[prefix[position]] == null)
+        {
+            return 0;
+        }
+
+        return this.next[prefix[position]].HowManyStartsWithPrefix(prefix, ++position);
+    }
+}
