4f1597312b968e9b4c8f0aee321dc0f7fb9217ec
Elena <fluffy.jesus@yandex.ru>
2024-03-26 22:08:47 +0300

    add tests & XML docs

--- SecondSemester/ParseTree/Program.cs
+++ SecondSemester/ParseTree/Program.cs
@@ -14,5 +14,15 @@ catch (FileNotFoundException)
 }
 catch (IncorrectInputException)
 {
-    Console.WriteLine("The expression for parse tree isn't correct.");
+    Console.WriteLine(
+        """
+        The expression for parse tree isn't correct. Please write it as follows:
+        (<operator> <operand_1> <operand_2>),
+        where <operand_1> and <operand_2> are either trees or numbers.
+        """);
+    Console.WriteLine("For example: (+ (* 1 2) 2)");
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine("Division by zero is forbidden.");
 }
